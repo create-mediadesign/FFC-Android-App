f7e57a64f1dadf90bee5baf56125d04f31d3e8b0
Philipp Ullmann <philipp.ullmann@create.at>
2012-07-31 14:13:34 +0200

    Documentation of settings activity

--- src/at/create/android/ffc/SettingsActivity.java
+++ src/at/create/android/ffc/SettingsActivity.java
@@ -1,10 +1,16 @@
 package at.create.android.ffc;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 /**
  * @author Philipp Ullmann
+ * Setting of username, password and URL to Fat Free CRM web application.
  */
 public final class SettingsActivity extends PreferenceActivity {
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
 }
