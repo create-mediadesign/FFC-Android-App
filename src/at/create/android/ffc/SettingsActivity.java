c57cb5cc2a633cc1b127f88099bcdc8d9af490ed
Philipp Ullmann <ullmann.philipp@gmail.com>
2012-07-31 08:04:36 +0200

    App settings

---
+++ src/at/create/android/ffc/SettingsActivity.java
@@ -0,0 +1,10 @@
+package at.create.android.ffc;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * @author Philipp Ullmann
+ */
+public final class SettingsActivity extends PreferenceActivity {
+    
+}
