2f94c0aeac065dff78ea4a56a7ea0ea1fcfb5ed1
Philipp Ullmann <philipp.ullmann@create.at>
2012-08-01 13:42:16 +0200

    Form based authentication

---
+++ src/at/create/android/ffc/FormBasedAuthentication.java
@@ -0,0 +1,52 @@
+package at.create.android.ffc;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Philipp Ullmann
+ * Authenticate with username and password.
+ */
+public final class FormBasedAuthentication {
+    private final String username;
+    private final String password;
+    private final String url;
+    
+    /**
+     * Stores the given parameters into instance variables.
+     * @param username Username
+     * @param password Password
+     * @param url Url to Fat Free CRM web application
+     */
+    public FormBasedAuthentication(final String username, final String password, final String url) {
+        this.username = username;
+        this.password = password;
+        this.url      = url;
+    }
+    
+    /**
+     * Authentication via username and password.
+     * @return True if the authentication succeeded, otherwise false is returned.
+     */
+    public boolean authenticate() {
+        MultiValueMap<String, Object> formData = new LinkedMultiValueMap<String, Object>();
+        formData.add("authentication[username]", username);
+        formData.add("authentication[password]", password);
+        formData.add("authentication[remember_me]", "1");
+        
+        HttpHeaders requestHeaders                              = new HttpHeaders();
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(formData, requestHeaders);
+        RestTemplate restTemplate                               = new RestTemplate();
+        ResponseEntity<String> response                         = restTemplate.exchange(url,
+                                                                                        HttpMethod.POST,
+                                                                                        requestEntity,
+                                                                                        String.class);
+        System.out.println(response.getBody());
+        return false;
+    }
+}
---
+++ src/at/create/android/ffc/test/FormBasedAuthenticationTest.java
@@ -0,0 +1,12 @@
+package at.create.android.ffc.test;
+
+import android.R;
+import android.test.AndroidTestCase;
+import at.create.android.ffc.FormBasedAuthentication;
+
+public final class FormBasedAuthenticationTest extends AndroidTestCase {
+    public void testSuccessfulAuthentication() {
+        FormBasedAuthentication auth = new FormBasedAuthentication(username, password, url)
+        
+    }
+}
