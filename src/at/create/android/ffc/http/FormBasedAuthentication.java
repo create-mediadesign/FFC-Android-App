ff55e9f788cfcdfc440724be64022787e404616e
Philipp Ullmann <philipp.ullmann@create.at>
2012-08-09 08:47:15 +0200

    Use "FormHttpMessageConverter" for form based requests

--- src/at/create/android/ffc/http/FormBasedAuthentication.java
+++ src/at/create/android/ffc/http/FormBasedAuthentication.java
@@ -1,8 +1,8 @@
 package at.create.android.ffc.http;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import java.net.URI;
+
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -37,16 +37,14 @@ public final class FormBasedAuthentication extends HttpBase {
      * @return True if the authentication succeeded, otherwise false is returned.
      */
     public boolean authenticate() {
-        MultiValueMap<String, Object> formData = new LinkedMultiValueMap<String, Object>();
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<String, String>();
         formData.add("authentication[username]", username);
         formData.add("authentication[password]", password);
         formData.add("authentication[remember_me]", "1");
         
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(formData, requestHeaders);
-        ResponseEntity<String> response                         = restTemplate.exchange(getUrl(),
-                                                                                        HttpMethod.POST,
-                                                                                        requestEntity,
-                                                                                        String.class);
-        return !response.getHeaders().getLocation().getPath().equals("/login");
+        restTemplate.getMessageConverters().add(new FormHttpMessageConverter());
+        URI uri = restTemplate.postForLocation(getUrl(), formData);
+        
+        return !uri.getPath().equals("/login");
     }
 }
