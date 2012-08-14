90bdd7ddbc47efc28071e01462c7fb4d0ea68a8e
Philipp Ullmann <philipp.ullmann@create.at>
2012-08-14 14:45:24 +0200

    Login activity

--- src/at/create/android/ffc/activity/MainActivity.java
+++ src/at/create/android/ffc/activity/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
     
     @Override
