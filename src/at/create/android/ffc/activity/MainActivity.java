794b68542985b7005980b7c7a5cca22cc75a7b17
Philipp Ullmann <philipp.ullmann@create.at>
2012-08-17 10:43:09 +0200

    Display login activity

--- src/at/create/android/ffc/activity/MainActivity.java
+++ src/at/create/android/ffc/activity/MainActivity.java
@@ -2,7 +2,6 @@ package at.create.android.ffc.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import at.create.android.ffc.R;
 
 public class MainActivity extends Activity {
@@ -10,11 +9,5 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
     }
 }
