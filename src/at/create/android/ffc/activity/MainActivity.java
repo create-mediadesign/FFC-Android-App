71314123603664eddb5dccf95a9c3771ce7f380d
Philipp Ullmann <ullmann.philipp@gmail.com>
2012-08-20 08:02:15 +0200

    Init of login fields

--- src/at/create/android/ffc/activity/MainActivity.java
+++ src/at/create/android/ffc/activity/MainActivity.java
@@ -1,10 +1,13 @@
 package at.create.android.ffc.activity;
 
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.app.Activity;
 import at.create.android.ffc.R;
+import at.create.android.ffc.domain.Setting;
 
 public class MainActivity extends Activity {
     private EditText urlField;
@@ -17,6 +20,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         findViews();
+        initFields(savedInstanceState);
     }
     
     @Override
@@ -38,7 +42,35 @@ public class MainActivity extends Activity {
         loginButton     = (Button) findViewById(R.id.sign_in_button);
     }
     
-    private void initFields() {
+    private void initFields(Bundle savedInstanceState) {
+        Setting setting = null;
         
+        // Load settings from "assets/setting.xml" file.
+        try {
+            setting = Setting.load(getAssets().open("setting.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        // Set url.
+        if (savedInstanceState.containsKey("url")) {
+            urlField.setText(savedInstanceState.getString("url"));
+        } else if (setting != null) {
+            urlField.setText(setting.getBaseUri());
+        }
+        
+        // Set username.
+        if (savedInstanceState.containsKey("username")) {
+            usernameField.setText(savedInstanceState.getString("username"));
+        } else if (setting != null) {
+            usernameField.setText(setting.getUsername());
+        }
+        
+        // Set password.
+        if (savedInstanceState.containsKey("password")) {
+            passwordField.setText(savedInstanceState.getString("password"));
+        } else if (setting != null) {
+            passwordField.setText(setting.getPassword());
+        }
     }
 }
